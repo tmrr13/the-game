c2ff56a36ecc0814d9beb9f96711f46decb1fd44
Egor_Shnel <egor_shnel@epam.com>
2019-08-14 20:06:49 +0300

    add status

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -14,10 +14,18 @@ export class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if(calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
       xIsNext: !this.state.xIsNext,
+    });
+
+    squares.some(function(entry) {console.log(entry)
+
     });
   }
 
@@ -30,8 +38,19 @@ export class Board extends React.Component {
     );
   }
 
+
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+
+    let status;
+     if(winner === "asdf") {
+       status = "BLYDIDANAKONECTO";
+     }
+      else if(winner === "X" || winner === "O"){
+       status = "Wins " +winner ;
+    } else if(!winner )
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+
     return (
       <div>
         <div className="status">{status}</div>
@@ -54,3 +73,33 @@ export class Board extends React.Component {
     );
   }
 }
+
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+    // console.log(lines)
+  }
+
+  const draw = squares.every(i => i != null);
+  if (draw) {
+    return "asdf";
+  }
+  return null;
+}
+
+// every
